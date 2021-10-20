9890c67f02b04c6ca748211fe0553e6e7ed2de15
Tesileanu Alexandru <tesileanualexandru@gmail.com>
2021-10-20 18:25:13 +0300

    Porneste La Input

---
+++ Key_Pressed/Key_Pressed.ino
@@ -0,0 +1,46 @@
+#include <Stepper.h>
+#include <Keyboard.h>
+
+
+const float STEPS_PER_REV = 42;
+const float GEAR_RED = 64;
+const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
+int StepsRequired;
+int dir = 12;
+Stepper steppermotor1(STEPS_PER_REV,3,4);
+Stepper steppermotor2(STEPS_PER_REV,5,4);
+Stepper steppermotor3(STEPS_PER_REV,7,4);
+Stepper steppermotor4(STEPS_PER_REV,9,4);
+Stepper steppermotor5(STEPS_PER_REV,6,4);
+Stepper steppermotor6(STEPS_PER_REV,8,4);
+Stepper steppermotor7(STEPS_PER_REV,10,4);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,INPUT);
+  pinMode(12,OUTPUT);
+  Serial.begin(9600); 
+}
+
+void loop() {
+   waitForSerial();
+    
+//  if (digitalRead(2) == HIGH) {
+//    digitalWrite(12,LOW); 
+//  }
+//  else {
+//    digitalWrite(12,LOW);
+//  }
+  steppermotor1.setSpeed(3000);
+  StepsRequired = 1900;
+  steppermotor1.step(StepsRequired); 
+  delay(500) ;
+}
+
+void waitForSerial(){
+  while (!Serial.available()) {
+  }
+  Serial.println(Serial.read());
+  
+}
